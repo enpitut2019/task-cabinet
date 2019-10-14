4038804a67435a1b144cc901039aecd4fcada6d5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-14 13:59:23 +0900

    Revert "タスク一覧のコンポーネントを追加"
    This reverts commit 06a98d659d248d015aa3c50ba625bbfb3991410c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TaskComponent } from './task.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TaskComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
