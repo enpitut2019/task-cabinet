4658af373d5c5190c96ae9515c756b0b87c77ffd
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-14 14:01:08 +0900

    タスク一覧のコンポーネントを PageTaskComponent として追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageTaskComponent } from './page-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageTaskComponent
   ],
   imports: [
     BrowserModule,
