f874b0a5c954474202117a81347083e1ed3d69d7
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-15 19:11:03 +0900

    タスク一覧を返すメソッドを追加

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Task } from './task';
+import { Observable, of } from 'rxjs';
+import { MOCK_TASK_LIST } from './mock-task-list';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+
+  getTaskList(): Observable<Task[]> {
+    return of(MOCK_TASK_LIST);
+  }
+
 }
