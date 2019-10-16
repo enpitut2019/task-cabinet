82c8675723e55fd02cd05a0f743b89dffbde06e7
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-16 13:06:00 +0900

    ルーティングを設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageTaskComponent } from './page-task.component';
+import { PageTaskAddComponent } from './page-task-add.component';
 
 
 const routes: Routes = [
-  { path: '', component: PageTaskComponent }
+  { path: '', component: PageTaskComponent },
+  { path: 'add', component: PageTaskAddComponent },
 ];
 
 @NgModule({
