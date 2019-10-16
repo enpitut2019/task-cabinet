c8509334bc211d654cf59f3e9a9c0fa0c1ce2df5
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-16 13:04:11 +0900

    custom elements でテストが通るように修正

--- src/app/page-task-add.component.spec.ts
+++ src/app/page-task-add.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageTaskAddComponent } from './page-task-add.component';
@@ -8,7 +9,8 @@ describe('PageTaskAddComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageTaskAddComponent ]
+      declarations: [ PageTaskAddComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
   }));
