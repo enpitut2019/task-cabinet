4c56997b3743da60e956a993a9b13b6b199aa817
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-10-16 10:19:12 +0900

    レイアウト用コンポーネントの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { PageTaskComponent } from './page-task.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { LayoutComponent } from './layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageTaskComponent
+    PageTaskComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
