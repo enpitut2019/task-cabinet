f8b355e1fe0a38a03b60bef810249a89a60c378f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2019-11-12 13:50:00 +0900

    set routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageTaskComponent } from './page-task.component';
 import { PageTaskAddComponent } from './page-task-add.component';
+import { PageSignInComponent } from './page-sign-in.component';
 
 
 const routes: Routes = [
   { path: '', component: PageTaskComponent },
   { path: 'add', component: PageTaskAddComponent },
+  { path: 'sign-in', component: PageSignInComponent },
 ];
 
 @NgModule({
