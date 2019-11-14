53b2bb2ae25ccd977d93c3b4faa49407f2834c71
高木颯汰 <kozamurai882@gmail.com>
2019-11-14 21:06:19 +0900

    fix test

--- src/app/page-top.component.spec.ts
+++ src/app/page-top.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { PageTopComponent } from './page-top.component';
 
@@ -8,6 +9,7 @@ describe('PageTopComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ PageTopComponent ]
     })
     .compileComponents();
