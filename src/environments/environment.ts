9a2f50e8cc78364733bfa6847b868751ff1e0276
HayataTakagi <h.takagi.work@gmail.com>
2019-12-19 12:40:13 +0900

    vapIdPublicKeyをハードコーディング

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4200',  // Proxyでサーバーに転送する(CORS対策)
-  vapIdPublicKey: 'YourSecretKey' // TODO: 機密情報の扱い
+  vapIdPublicKey: 'BKC5-9mK29h2-ryo14BqYqppcN0HtDwFyNZLfmiMvumImyC3sKaWAt_gabaY_u-ffxC2uUDeJcmlBgDpHuo6FBE='
 };
 
 /*
