d5b634b0a27baaeed4ac725febb50c6777e378bc
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-15 19:05:58 +0900

    タスクの表示

--- src/app/page-task-info.component.ts
+++ src/app/page-task-info.component.ts
@@ -3,7 +3,9 @@ import { Task } from './models/task';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { TaskService } from './services/task.service';
 import { switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, timer } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { AlertService } from './services/alert.service';
 
 @Component({
   selector: 'app-page-task-info',
@@ -16,7 +18,10 @@ export class PageTaskInfoComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe,
     private taskService: TaskService,
+    public alertService: AlertService
   ) { }
 
   ngOnInit() {
@@ -37,7 +42,30 @@ export class PageTaskInfoComponent implements OnInit {
     ).subscribe((task: Task) => {
       this.task = task;
       console.log(task);
+    }, error => {
+      this.alertService.showErrorAlert('タスクの取得に失敗しました。');
     });
+  }
+
+  doneTask(task: Task) {
+    this.taskService.doneTask(task).subscribe(() => {
+      this.router.navigate(['task']);
+    }, err => {
+      console.error(err);
+      this.alertService.showErrorAlert('タスクの完了に失敗しました。');
+    });
+  }
+
+  formatDatetime(datetime: Date) {
+    const q = new Date(datetime);
+    const now = new Date();
+    const dayNames = ['日', '月', '火', '水', '木', '金', '土'];
+    const qDayName = dayNames[q.getDay()];
+    if (q.getFullYear() === now.getFullYear()) {
+      return  this.datePipe.transform(q, `M月d日(${qDayName}) HH:mm`);
+    } else {
+      return  this.datePipe.transform(q, `yyyy年M月d日(${qDayName}) HH:mm`);
+    }
   }
 
 }
