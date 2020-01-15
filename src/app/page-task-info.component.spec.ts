6208244f774d89a7114ed1c568fc90c948a71e8f
tmyksj <33417830+tmyksj@users.noreply.github.com>
2020-01-15 18:34:32 +0900

    タスクを取得

--- src/app/page-task-info.component.spec.ts
+++ src/app/page-task-info.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageTaskInfoComponent } from './page-task-info.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AlertService } from './services/alert.service';
+import { CookieService } from 'ngx-cookie-service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('PageTaskInfoComponent', () => {
   let component: PageTaskInfoComponent;
@@ -8,7 +14,14 @@ describe('PageTaskInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PageTaskInfoComponent ]
+      declarations: [ PageTaskInfoComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+      imports: [
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers: [AlertService, CookieService],
     })
     .compileComponents();
   }));
--- src/app/page-task-info.component.ts
+++ src/app/page-task-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from './models/task';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { TaskService } from './services/task.service';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-page-task-info',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageTaskInfoComponent implements OnInit {
 
-  constructor() { }
+  task: Task;
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService,
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.pipe(
+      switchMap((paramMap: ParamMap) => {
+        if (paramMap.has('id')) {
+          return this.taskService.getTask(parseInt(paramMap.get('id'), 10));
+        } else {
+          return of({
+            id: null,
+            name: '',
+            deadline: new Date(),
+            finishedAt: null,
+            estimate: 1,
+          });
+        }
+      })
+    ).subscribe((task: Task) => {
+      this.task = task;
+      console.log(task);
+    });
   }
 
 }
